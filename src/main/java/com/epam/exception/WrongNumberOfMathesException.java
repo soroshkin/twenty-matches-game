3f207ce8017c1ba96f3120b9f847175d8b6ba387
Soroshkin Anton <soroshkin_a_m@mail.ru>
2020-01-07 21:39:23 +0300

    Create custom exception class

---
+++ src/main/java/com/epam/exception/WrongNumberOfMathesException.java
@@ -0,0 +1,7 @@
+package com.epam.exception;
+
+public class WrongNumberOfMathesException extends Exception {
+    public WrongNumberOfMathesException(String message) {
+        super(message);
+    }
+}
